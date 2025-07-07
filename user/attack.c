29f54102cba7275f9c75030ca985c3beaa76099b
TJUer2964 <747968479@qq.com>
2025-07-07 15:32:21 +0800

    feat: implement trace and attack, complete the whole syscall lab.

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,13 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  // your code here.  you should write the secret to fd 2 using write
+  // (e.g., write(2, secret, 8)
+  char *end = sbrk(PGSIZE * 32);
+  // 不能理解！这个16是怎么来的？？
+  // 我也不能理解。。。
+  end = end + 16 * PGSIZE;
+  char *secret = end + 32;
+  write(2, secret, 8);
   exit(1);
 }
